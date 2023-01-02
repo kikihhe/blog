326f0b17529f82f96c7d0269819cee514348507d
2382546457 <2382546457@qq.com>
2023-01-02 20:38:55 +0800

    项目架构

---
+++ sangeng-blog/src/main/java/com/xiaohe/SanGengBlogApplication.java
@@ -0,0 +1,14 @@
+package com.xiaohe;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SanGengBlogApplication {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext app = SpringApplication.run(SanGengBlogApplication.class, args);
+
+    }
+}
